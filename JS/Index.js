c503e8e5220ebc214e7ab12be43975905e6c09fd
KevinJJ01 <munozcastanedakevinjulian@gmail.com>
2024-12-05 13:39:53 -0500

    Solution script and implementation for more design

---
+++ JS/Index.js
@@ -0,0 +1,24 @@
+emailjs.init("e2Sl3H41c1FHJlRF7"); // Reemplaza con tu ID de usuario
+
+document.querySelector(".form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const form = event.target;
+  emailjs.sendForm("service_6d0nxod", "template_qgx1kbb", form).then(
+    function (response) {
+      alert("¡Correo enviado con éxito!");
+    },
+    function (error) {
+      alert("Hubo un error al enviar el correo.");
+    }
+  );
+});
+
+function toggleMenu() {
+  var menuContent = document.querySelector('.menu-content');
+  if (menuContent.style.display === "block") {
+      menuContent.style.display = "none";
+  } else {
+      menuContent.style.display = "block";
+  }
+}
